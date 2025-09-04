dcf02ee5a63d26221cac2a3ae3261def05022a99
jhimydev-rx <jhimyreyesdev@gmail.com>
2025-09-03 19:48:32 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/lab03/cps/lab03/Application.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.tecsup.lab03.cps.lab03;
+
+/**
+ *
+ * @author chboy
+ */
+public class Application {
+    
+}
---
+++ src/main/java/pe/edu/tecsup/lab03/cps/lab03/CpsLab03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.edu.tecsup.lab03.cps.lab03;
+
+/**
+ *
+ * @author chboy
+ */
+public class CpsLab03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
