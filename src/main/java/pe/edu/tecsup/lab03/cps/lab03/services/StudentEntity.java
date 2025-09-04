33410e321edef2446a6efe60c6e4d0fc116a655d
jhimydev-rx <jhimyreyesdev@gmail.com>
2025-09-03 20:50:27 -0500

    Second commit-colaborador

---
+++ src/main/java/pe/edu/tecsup/lab03/cps/lab03/services/StudentEntity.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.tecsup.lab03.cps.lab03.services;
+
+/**
+ *
+ * @author chboy
+ */
+public class StudentEntity {
+    
+}
